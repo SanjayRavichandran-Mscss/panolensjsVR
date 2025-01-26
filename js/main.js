7dd1fe34a54a48db082e0e509e3eaa93dff5a83d
SanjayRavichandran-Mscss <117557388+SanjayRavichandran-Mscss@users.noreply.github.com>
2025-01-26 11:46:55 +0530

    add panorama video

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-const panorama = new PANOLENS.ImagePanorama( 'images/panel1.jpeg' );
+const panorama = new PANOLENS.ImagePanorama( 'images/panel2.mp4' );
 const panorama2 = new PANOLENS.ImagePanorama('images/pano5.jpg');
+const panorama3 = new PANOLENS.ImagePanorama('images/panel1.jpeg');
 let imageContainer = document.querySelector('.image-container')
 
 
@@ -18,6 +19,7 @@ const viewer = new PANOLENS.Viewer({
 
 panorama.link( panorama2, infospotPositions[0]);
 panorama2.link( panorama, infospotPositions[1]);
+panorama3.link( panorama, infospotPositions[2]);
 
 viewer.add( panorama,panorama2 );
 
